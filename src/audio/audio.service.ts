0aa784a1c7637742f251047686f456204d475a0d
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-05-05 16:29:44 +0300

    update audio generate logic

--- src/audio/audio.service.ts
+++ src/audio/audio.service.ts
@@ -10,57 +10,62 @@ import AudioDto from './dto/audio.dto';
 import { instance } from 'src/utils/axios';
 import * as fs from 'fs';
 import { Errors } from 'src/constants/errors';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AudioService {
   constructor(
     @InjectRepository(AudioEntity)
     private audioRepository: Repository<AudioEntity>,
+    private userRepository: Repository<UserEntity>,
   ) {}
   async createAudio(audioBody: AudioDto, user) {
-    try {
-      const url = `audio/generate/?labeled_text=${encodeURIComponent(
-        audioBody.markup,
-      )}`;
-      const audioGenerateResponse = await instance.post(url);
-      const jobId = audioGenerateResponse.data['job_id'];
-      let fileUrl;
-      await this.getFileUrlFromJobStatusResponse(fileUrl, jobId);
-      const audioFileResponse = await instance.get(`audio/get/${fileUrl}`, {
-        responseType: 'arraybuffer',
-      });
-      const audioData: Buffer = Buffer.from(audioFileResponse.data);
-      const filename = `./db_audiofiles/audio_${Date.now()}.wav`;
-      await this.saveAudioToFile(audioData, filename);
-      const audioEntity = {
-        markup: audioBody.markup,
-        path: filename,
-        user,
-      };
-      return this.audioRepository.save(audioEntity);
-    } catch (error) {
-      throw new BadRequestException(Errors.SERVER_ERROR);
-    }
-  }
-  async getFileUrlFromJobStatusResponse(fileUrl, jobId) {
-    let count = 0;
-    while (!fileUrl) {
+    if (user.tokens >= 1) {
       try {
-        const jobStatusResponse = await instance.get(`audio/job/${jobId}`);
-        console.log(jobStatusResponse.data);
-        if (jobStatusResponse.status === 200) {
-          fileUrl = jobStatusResponse.data['file_url'];
-        } else {
-          console.log(
-            `Try to get job status response ${++count}: Response status ${
-              jobStatusResponse.status
-            }`,
-          );
+        const url = `audio/generate/?labeled_text=${encodeURIComponent(
+          audioBody.markup,
+        )}`;
+        const audioGenerateResponse = await instance.post(url);
+        const jobId = audioGenerateResponse.data['job_id'];
+        let fileUrl;
+        let count = 0;
+        while (!fileUrl) {
+          try {
+            const jobStatusResponse = await instance.get(`audio/job/${jobId}`);
+            console.log(jobStatusResponse.data);
+            if (jobStatusResponse.status === 200) {
+              fileUrl = jobStatusResponse.data['file_url'];
+            } else {
+              console.log(
+                `Try to get job status response ${++count}: Response status ${
+                  jobStatusResponse.status
+                }`,
+              );
+            }
+          } catch (error) {
+            throw new BadRequestException(Errors.AUDIO_CREATE_ERROR);
+          }
+          await new Promise((resolve) => setTimeout(resolve, 5000));
         }
+        const audioFileResponse = await instance.get(`audio/get/${fileUrl}`, {
+          responseType: 'arraybuffer',
+        });
+        const audioData: Buffer = Buffer.from(audioFileResponse.data);
+        const filename = `./db_audiofiles/audio_${Date.now()}.wav`;
+        await this.saveAudioToFile(audioData, filename);
+        const audioEntity = {
+          markup: audioBody.markup,
+          path: filename,
+          user,
+        };
+        user.tokens -= 1;
+        this.userRepository.update(user.id, user);
+        return this.audioRepository.save(audioEntity);
       } catch (error) {
-        throw new BadRequestException(Errors.AUDIO_CREATE_ERROR);
+        throw new BadRequestException(Errors.SERVER_ERROR);
       }
-      await new Promise((resolve) => setTimeout(resolve, 5000));
+    } else {
+      throw new BadRequestException('Недостаточно токенов для генерации');
     }
   }
   async saveAudioToFile(audioData: Buffer, filename: string): Promise<void> {
